a94446f64776641ef788adbee0c8a4c1d32bf196
Shubham Ayachit <shubham.s.ayachit@gmail.com>
2022-07-01 05:29:02 -0400

    Seperated Expense Date componant

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,14 +1,11 @@
 import './ExpenseItem.css'
+import ExpenseDate from './ExpenseDate';
 
 function ExpenseItem(props) {
 
-  const expenseDate = props
-  const expenseTitle = 'Car Insurance';
-  const expenseAmount = 294.94;
-
   return (
     <div className='expense-item'>
-      <div>{props.date.toISOString()}</div>
+     <ExpenseDate date={props.date} />
       <div className='expense-item__description'>
         <h2>{props.title}</h2>
         <div className='expense-item__price'>${props.amount}</div>
